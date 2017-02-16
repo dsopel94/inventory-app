b3687ca1dedf938fbe1b8d198cf9fec7aa8d115b
Damian Sopel <dsopel0@gmail.com>
2017-02-15 21:29:24 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,16 @@
+var products = [
+	{
+		name: "Aluminum Phosphate",
+		location: "Laboratory",
+		quantity: "0",
+		imageUrl: "http://lorempixel.com/100/100",
+		form: "Solid",
+		dateAdded: null,
+		hazardous: true 
+	},
+];
+
+var locations = ['Laboratory', 'First floor', 'Second floor', 
+			     'Basement']
+
+var forms = ['liquid', 'solid', 'powder', 'other'] 
